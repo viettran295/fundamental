e0b6e3e4005f23d30af5dd11da6b27531408c423
viettr <viettran295@gmail.com>
2025-12-14 13:04:58 +0100

    Add get SIC data

--- src/financial_stmt/sec_client.rs
+++ src/financial_stmt/sec_client.rs
@@ -1,6 +1,5 @@
 use crate::interface::HttpClient;
 
-use log::warn;
 use reqwest::header::{HeaderMap, HeaderValue, USER_AGENT};
 use serde_json::Value;
 
@@ -39,6 +38,13 @@ struct CompanyTickers {
 }
 
 #[derive(Debug, serde::Deserialize)]
+pub struct StandardIndustryCode {
+    sic: String,
+    #[serde(rename = "sicDescription")]
+    sic_description: String,
+}
+
+#[derive(Debug, serde::Deserialize)]
 struct SecResponse {
     #[allow(dead_code)]
     pub fields: Vec<String>,
@@ -60,7 +66,23 @@ impl SecClient {
         }
     }
 
-    async fn ticker_to_cik(search_ticker: &str) -> Result<Option<u32>, Box<dyn std::error::Error>> {
+    /// Fetch company's metadata Standard Industry Code (SIC)
+    pub async fn fetch_sic(&self) -> Result<StandardIndustryCode, Box<dyn std::error::Error>> {
+        let cik = Self::ticker_to_cik(&self.ticker).await?;
+        let url = format!(
+            "https://data.sec.gov/submissions/{}.json",
+            cik.unwrap_or_default()
+        );
+        let client = self.http_client.client();
+        let response = client.get(url).send().await?;
+        let response = response.error_for_status()?;
+        let data: StandardIndustryCode = response.json().await?;
+        Ok(data)
+    }
+
+    async fn ticker_to_cik(
+        search_ticker: &str,
+    ) -> Result<Option<String>, Box<dyn std::error::Error>> {
         let url = "https://www.sec.gov/files/company_tickers_exchange.json";
         let client = reqwest::Client::builder()
             .user_agent("(contact@example.com)")
@@ -71,7 +93,7 @@ impl SecClient {
         let company_tickers: Vec<CompanyTickers> = sec_response.data;
         for company_ticker in company_tickers {
             if company_ticker.ticker.unwrap_or(String::from("")) == search_ticker {
-                return Ok(Some(company_ticker.cik));
+                return Ok(Some(format!("CIK000{}", company_ticker.cik)));
             }
         }
         Ok(None)
@@ -82,16 +104,11 @@ impl HttpClient<serde_json::Value> for SecClient {
     type Error = reqwest::Error;
 
     async fn fetch_data(&self) -> Result<Value, Self::Error> {
-        let cik = match Self::ticker_to_cik(&self.ticker).await {
-            Ok(Some(found_cik)) => {
-                format!("CIK000{}", found_cik)
-            }
-            Ok(None) | Err(_) => {
-                warn!("Ticker not fount CIK");
-                String::from("")
-            }
-        };
-        let url = format!("https://data.sec.gov/api/xbrl/companyfacts/{}.json", cik);
+        let cik = Self::ticker_to_cik(&self.ticker).await.unwrap_or_default();
+        let url = format!(
+            "https://data.sec.gov/api/xbrl/companyfacts/{}.json",
+            cik.unwrap_or_default()
+        );
         let client = self.http_client.client();
         let response = client.get(url).send().await?;
         let response = response.error_for_status()?;
