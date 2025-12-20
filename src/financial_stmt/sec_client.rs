f3e0a4e61da84f51bb33e45eb5280650a37a668f
viettr <viettran295@gmail.com>
2025-12-20 10:18:02 +0100

    Add fetch company tickers

--- src/financial_stmt/sec_client.rs
+++ src/financial_stmt/sec_client.rs
@@ -1,12 +1,12 @@
+use std::collections::HashMap;
+
 use crate::interface::HttpClient;
 
-use reqwest::{
-    Response,
-    header::{HeaderMap, HeaderValue, USER_AGENT},
-};
+use log::debug;
+use reqwest::header::{HeaderMap, HeaderValue, USER_AGENT};
 use serde_json::Value;
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct ConfiguredHttpClient {
     client: reqwest::Client,
 }
@@ -31,31 +31,38 @@ impl ConfiguredHttpClient {
 }
 
 #[derive(Debug, serde::Deserialize)]
-struct CompanyTickers {
-    cik: u32,
+pub struct CompanyTickersExchange {
+    pub cik: u32,
     #[allow(dead_code)]
-    name: Option<String>,
-    ticker: Option<String>,
+    pub name: Option<String>,
+    pub ticker: Option<String>,
     #[allow(dead_code)]
-    exchange: Option<String>,
+    pub exchange: Option<String>,
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub struct CompanyTickers {
+    pub cik_str: u32,
+    pub ticker: Option<String>,
+    pub title: Option<String>,
 }
 
 #[derive(Debug, serde::Deserialize)]
 pub struct SICResponse {
-    sic: String,
+    pub sic: String,
     #[serde(rename = "sicDescription")]
-    sic_description: String,
+    pub sic_description: String,
 }
 
 #[derive(Debug, serde::Deserialize)]
 struct SecResponse {
     #[allow(dead_code)]
     pub fields: Vec<String>,
-    pub data: Vec<CompanyTickers>,
+    pub data: Vec<CompanyTickersExchange>,
 }
 
 /// Client fetches data from SEC API
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct SecClient {
     ticker: String,
     http_client: ConfiguredHttpClient,
@@ -64,6 +71,8 @@ pub struct SecClient {
 impl SecClient {
     /// SEC API endpoint for company ticker, which is used for ticker-to-CIK mapping
     const TICKER_LOOKUP_URL: &str = "https://www.sec.gov/files/company_tickers_exchange.json";
+    /// Simplified version of TICKER_LOOKUP_URL
+    const COMPANY_TICKERS_SIMPLIFIED: &str = "https://www.sec.gov/files/company_tickers.json";
     /// Base URL for submission data
     const SUBMISSIONS_BASE_URL: &str = "https://data.sec.gov/submissions";
     /// Base URL for company facts data
@@ -76,6 +85,10 @@ impl SecClient {
         }
     }
 
+    pub fn set_ticker(&mut self, ticker: String) {
+        self.ticker = ticker;
+    }
+
     /// Fetch company's metadata Standard Industry Code (SIC)
     pub async fn fetch_sic(&self) -> Result<SICResponse, Box<dyn std::error::Error>> {
         let cik = self.ticker_to_cik().await?;
@@ -84,19 +97,40 @@ impl SecClient {
             Self::SUBMISSIONS_BASE_URL,
             cik.unwrap_or_default()
         );
+        debug!("Fetching SIC");
         let data: SICResponse = self.fetch_json(&url).await?;
         Ok(data)
     }
 
+    pub async fn fetch_all_company_tickers(
+        &self,
+    ) -> Result<HashMap<String, CompanyTickers>, Box<dyn std::error::Error>> {
+        type CompanyMap = HashMap<String, CompanyTickers>;
+        debug!("Fetching all company tickers");
+        let json_response: CompanyMap = self.fetch_json(Self::COMPANY_TICKERS_SIMPLIFIED).await?;
+        Ok(json_response)
+    }
+
     async fn ticker_to_cik(&self) -> Result<Option<String>, Box<dyn std::error::Error>> {
         let sec_response: SecResponse = self.fetch_json(Self::TICKER_LOOKUP_URL).await?;
-        let company_tickers: Vec<CompanyTickers> = sec_response.data;
+        let company_tickers: Vec<CompanyTickersExchange> = sec_response.data;
         for company_ticker in company_tickers {
             if company_ticker.ticker.unwrap_or(String::from("")) == self.ticker {
-                return Ok(Some(format!("CIK000{}", company_ticker.cik)));
+                return Ok(Some(Self::add_cik_padding(company_ticker.cik)));
             }
         }
         Ok(None)
+    }
+    fn add_cik_padding(cik: u32) -> String {
+        let mut nr_digits = cik.to_string().len();
+        let mut padding_digits: String = String::from("");
+        let standard_cik_len = 10;
+
+        while nr_digits < standard_cik_len {
+            padding_digits += "0";
+            nr_digits += 1;
+        }
+        format!("CIK{}{}", padding_digits, cik)
     }
 }
 
